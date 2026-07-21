a002cb660e92478a129b360474cbb1fcac2be99c
Gopika-Ilamparithy <gopika622001@gmail.com>
2026-07-21 17:30:33 +0530

    Add project files.

---
+++ DataPassingInDotNetCore/DataPassingInDotNetCore/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using DataPassingInDotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace DataPassingInDotNetCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if(model.username == "user@gmail.com" && model.password == "134567")
+                {
+                    TempData["Message"] = "Welcome back";
+
+                    string strUser = JsonSerializer.Serialize(model);
+                    HttpContext.Session.SetString("User", strUser);
+
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Username or password doesn't exist!";
+                }
+                
+            }
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return View();
+        }
+    }
+}
---
+++ DataPassingInDotNetCore/DataPassingInDotNetCore/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataPassingInDotNetCore.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
