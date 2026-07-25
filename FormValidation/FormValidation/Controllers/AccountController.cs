b6b50f10eebdecc95e64121bb98e4f3e297ebdfc
Gopika-Ilamparithy <gopika622001@gmail.com>
2026-07-25 21:36:34 +0530

    Added SignUp Form along with Validations

---
+++ FormValidation/FormValidation/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using FormValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
---
+++ FormValidation/FormValidation/Models/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FormValidation.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormValidation.Models
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Contact number must be 10 digits")]
+        public string? Contact {  get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+
+        [ValidateCheckBox(ErrorMessage = "Please Accept Terms")]
+        public bool Terms { get; set; }
+
+    }
+}
